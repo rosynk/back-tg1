78f3fe0dbdb9f4dd5f625c5d256bdd61341a4b50
kleber192 <kleberygorsantosoficial@gmail.com>
2026-05-10 09:34:25 -0300

    Correcoes feitas no Eclipse

--- BiziBanco/Bizi/src/main/java/bizi/com/demo/login/RedefinirSenhaDto.java
+++
@@ -1,9 +0,0 @@
-package bizi.com.demo.login;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RedefinirSenhaDto(
-    @NotBlank String email,
-    @NotBlank String codigo,
-    @NotBlank String novaSenha
-) {}
--- BiziBanco/Bizi/src/main/java/bizi/com/demo/pagamentoBoleto/PagamentoBoletoRepository.java
+++
@@ -1,7 +0,0 @@
-package bizi.com.demo.pagamentoBoleto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoBoletoRepository extends JpaRepository<PagamentoBoletoModel, Long> {
-
-}
