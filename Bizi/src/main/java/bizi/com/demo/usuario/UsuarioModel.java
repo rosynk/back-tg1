b355bfd13688c92696c53e3efc6674520f127e82
Rosynk <beatrizrterra@gmail.com>
2026-05-12 15:50:21 -0300

    feat: ajuste para notificar o usuário que a conta está desabilitada ainda

--- Bizi/src/main/java/bizi/com/demo/usuario/UsuarioModel.java
+++ Bizi/src/main/java/bizi/com/demo/usuario/UsuarioModel.java
@@ -115,7 +115,7 @@ public class UsuarioModel implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+    	return this.ativo; 
     }
 
     // --- CONSTRUTORES, GETTERS E SETTERS ---
